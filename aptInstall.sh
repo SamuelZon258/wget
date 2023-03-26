562669fd1d14fda0be937c1ee4154029b7e0ae13
SamuelZon <zonsamuel@gmail.com>
2023-03-26 15:39:29 +0800

    Update aptInstall.sh

--- aptInstall.sh
+++ aptInstall.sh
@@ -1,1 +1,4 @@
-apt update && apt install curl vim aria2 lrzsz screen gzip sudo  -y
+apt update && apt install curl vim aria2 lrzsz screen gzip sudo -y &&\
+cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bk && echo "Port 22
+PasswordAuthentication yes
+PermitRootLogin yes">>/etc/ssh/sshd_config && sudo service ssh start && sudo update-rc.d ssh enable
